9613c2c75248bd2fd1a2790ec83115b04c9e43b5
ali-alim <ali.mamedov.2016@gmail.com>
2022-04-20 15:43:36 +0400

    build navbar template

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import "./navbar.css"
+
+export default function Navbar() {
+  return (
+    <div className="navbar">
+      <div className="navbar__left">Left</div>
+      <div className="navbar__center">Center</div>
+      <div className="navbar__right">Right</div>
+    </div>
+  );
+}
