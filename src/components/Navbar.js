8506282c4c006dd3830b981f4593c0e464ae7ece
ali-alim <ali.mamedov.2016@gmail.com>
2022-04-20 16:18:07 +0400

    add styles to navbar links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,16 @@
-import "./navbar.css"
+import "./navbar.scss";
 
 export default function Navbar() {
   return (
     <div className="navbar">
-      <div className="navbar__left">Left</div>
+      <div className="navbar__left">
+        {["women", "men", "kids"].map((item) => (
+          <p key={`link -${item}`}>
+            <a href={`#${item}`}>{item}</a>
+            <div />
+          </p>
+        ))}
+      </div>
       <div className="navbar__center">Center</div>
       <div className="navbar__right">Right</div>
     </div>
